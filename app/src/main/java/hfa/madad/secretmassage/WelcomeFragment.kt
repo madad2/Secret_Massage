e90b6ee4873ca4c50c95f1d04d3fee03cdc9872a
madad <alexander.laputko@ya.ru>
2023-12-03 19:41:05 +0300

    WelcomeFragment create.

---
+++ app/src/main/java/hfa/madad/secretmassage/WelcomeFragment.kt
@@ -0,0 +1,16 @@
+package hfa.madad.secretmassage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import hfa.madad.secretmassage.R
+
+class WelcomeFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+// Заполнение макета для этого фрагмента
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+}
